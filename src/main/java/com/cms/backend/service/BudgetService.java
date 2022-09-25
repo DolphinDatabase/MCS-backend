d6644aa4dfb2ae7fad28e82d1cae7011172f7bac
BEATRIZ MEDEIROS DE OLIVEIRA SANTOS <beatriz.medeiros@embraer.com.br>
2022-09-25 20:51:55 -0300

    put/ update budget

--- src/main/java/com/cms/backend/service/BudgetService.java
+++ src/main/java/com/cms/backend/service/BudgetService.java
@@ -11,10 +11,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.cms.backend.SummaryModel.BudgetSummaryModel;
 import com.cms.backend.SummaryModel.ResponseSummaryModel;
@@ -63,6 +66,27 @@ public class BudgetService {
         }
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseSummaryModel> editBudget(@PathVariable Long id, @RequestBody Budget budget){
+        ResponseSummaryModel res = new ResponseSummaryModel();
+        try {
+            Budget b = bRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+            b.setDescription(budget.getDescription());
+            b.setTotal(budget.getTotal());
+            res.setAll(200, true, "Budget " +b.getId()+"Updated", toBudgetSummaryModel(bRepository.save(b)));
+            logger.info(res.getMessage());
+            return ResponseEntity.status(HttpStatus.OK).body(res);
+        }catch(ResponseStatusException err){
+            res.setAll(404, false, "Budget "+id+" Not Found", null);
+            logger.info(res.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(res);
+        
+        } catch (Exception err) {
+            res.setAll(500, false, err.getMessage(), null);
+            logger.error(res.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+        }
+    }
 
     private BudgetSummaryModel toBudgetSummaryModel(Budget budget){
         return modelMapper.map(budget,BudgetSummaryModel.class);
